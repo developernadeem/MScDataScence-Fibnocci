a656d1498a91f672fad571dc9810833f6346838f
Muhammad Nadeem <nadeemgujjar71@gmail.com>
2022-02-03 14:08:21 +0000

    Initial Commit

---
+++ Fib.py
@@ -0,0 +1,16 @@
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n - 2) + fib(n - 1)
+
+
+# reads the input number
+def start():
+    s_number = input("Please input a number to calculate a Fibonacci number: ")
+    number = int(s_number)
+    if number > 0:
+        print(fib(number))
+        start()
+
+
+start()
